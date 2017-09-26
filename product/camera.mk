f4622855a5344aeccaae25761538063d5d3edd72
pavlaras <theodoroupaulos@gmail.com>
2017-09-27 00:17:53 +0200

    Needed in AOSCP

--- product/camera.mk
+++ product/camera.mk
@@ -1,5 +1,5 @@
 PRODUCT_PACKAGES += \
-    Snap
+    Camero
 
 PRODUCT_PACKAGES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
