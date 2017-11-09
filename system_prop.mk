b757b60369421fff94fb098f87b14ced4c529b8d
kessaras <theodoroupaulos@gmail.com>
2017-11-09 07:24:43 +0200

    Add some missing gps props

--- system_prop.mk
+++ system_prop.mk
@@ -54,7 +54,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.gps.qc_nlp_in_use=1 \
     persist.loc.nlp_name=com.qualcomm.services.location \
-    ro.gps.agps_provider=1 
+    ro.gps.agps_provider=1 \
+    ro.qc.sdk.izat.premium_enabled=0 \
+    ro.qc.sdk.izat.service_mask=0x0
 
 # NITZ
 PRODUCT_PROPERTY_OVERRIDES += \
