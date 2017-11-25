8bd04a6e828485f1c81313fc917d400ef2cfed37
pavlaras <theodoroupaulos@gmail.com>
2017-11-25 06:52:05 +0100

    Update mpctl

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -1,4 +1,8 @@
+# QC Perf
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true \
+    ro.qualcomm.cabl=2 \
+    ro.qualcomm.perf.cores_online=2 \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.min_freq_0=384000 \
     ro.min_freq_4=384000
