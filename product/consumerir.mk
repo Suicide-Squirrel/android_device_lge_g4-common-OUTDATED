40c84be3a05b4aece5f7d75b72974c19fe4a31db
pavlaras <theodoroupaulos@gmail.com>
2017-12-16 18:21:50 +0100

    Compile consumerir module
    Change-Id: Ie6a9ac142e434f44373fe2311816ccea6002a215

--- product/consumerir.mk
+++ product/consumerir.mk
@@ -1,5 +1,8 @@
+# Consumer IR
+TARGET_PROVIDES_CONSUMERIR_HAL := true
+
 PRODUCT_PACKAGES += \
-    consumerir.msm8992 
+    consumerir.msm8992
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml 
+    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
