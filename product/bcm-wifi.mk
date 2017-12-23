8271b6c33dd020c4ffd5d7af2b0b67f74ccc8dd2
kessaras <theodoroupaulos@gmail.com>
2017-12-23 07:27:32 +0200

    g4: compile wificond

--- product/bcm-wifi.mk
+++ product/bcm-wifi.mk
@@ -1,8 +1,10 @@
+# Wifi
 PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    wificond
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
