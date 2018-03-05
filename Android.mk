dca48f5467af78e3d70e5f31f18816c6fe39601c
steadfasterX <steadfastX@boun.cr>
2018-03-05 13:53:36 +0100

    new: adding all g4 models (unlocked by UsU)
    (cherry picked from commit 99bd296ac5213449d0430fdeb83b193b86141cc0)
    (cherry picked from commit 3224dc095e4bf88a1f18c73f21c58c993876c966)

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter g4 f500 h810 h811 h815 h818 vs986, $(TARGET_DEVICE)),)
+ifneq ($(filter g4 ls991 f500 h810 h811 h812 h815 h818 h819 us991 vs986, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
